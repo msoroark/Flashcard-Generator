4b3619830b3231a3fd730d74298ea46646d0f9ea
Meg O'Roark <msoroark@gmail.com>
2017-11-04 18:05:36 -0500

    constructors built

---
+++ ClozeCard.js
@@ -0,0 +1,10 @@
+
+var ClozeCard = function(text, cloze) {
+    this.cloze = cloze;
+    this.partial = "..." + partial;
+    this.fullText = [cloze] + " " +partial;
+
+
+}
+
+module.exports = ClozeCard;
