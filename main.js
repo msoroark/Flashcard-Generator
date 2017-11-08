a12cc6baa7280a0d4ba08d18aa6ddfb36ce513db
Meg O'Roark <msoroark@gmail.com>
2017-11-07 21:40:53 -0600

    Basic and Cloze card functions are working. Need Review function.

--- main.js
+++ main.js
@@ -15,45 +15,104 @@ var clozeCards = [];
 //Prompt for Cloze or Basic
 var chooseCard = function() {
     inquirer.prompt([{
-        name: 'Card',
-        message: 'Please enter the card type you would like to build',
-        type: 'list',
-        choices: [{
-            name: 'Basic'
-        }, {
-            name: 'Cloze'
-        }]
-    }])
-    .then(function(answers) {
+            name: 'Card',
+            message: 'Please enter the card type you would like to build',
+            type: 'list',
+            choices: [{
+                name: 'Basic'
+            }, {
+                name: 'Cloze'
+            }]
+        }])
+        .then(function(answers) {
 
-        if (answers.Card === 'Basic') {
-            addBasic();
-        } else if (answers.Card === 'Cloze') {
-            addCloze();
-        }
-    });
+            if (answers.Card === 'Basic') {
+                addBasic();
+            } else if (answers.Card === 'Cloze') {
+                addCloze();
+            }
+        });
 
 };
 
-chooseCard();
 
 //Function to add basic card
 var addBasic = function() {
     inquirer.prompt([{
-        name: 'Question',
-        message: "Enter a question"
-    }, {
-        name: 'Answer',
-        message: "Enter the correct answer"
+            name: 'question',
+            message: "Enter a question"
+        }, {
+            name: 'answer',
+            message: "Enter the correct answer"
+        }])
+        .then(function(answers) {
+
+            //variable holding created card. 
+            var newCard = new BasicCard(answers.question, answers.answer);
+
+            //push new card to basic card array
+            basicCards.push(newCard);
+
+            console.log("\nBasic card created \nQuestion: " + answers.question +
+                "\nAnswer: " + answers.answer);
+
+            console.log(basicCards);
+
+            whatsNext();
+
+        })
+};
+
+//Function for a Cloze card
+var addCloze = function() {
+
+
+    inquirer.prompt([{
+            name: 'text',
+            message: 'Provide the full text',
+        }, {
+            name: 'cloze',
+            message: 'What is the cloze portion?',
     }])
     .then(function(answers) {
+    
+    //Hold new clozecard constructor
+    var newCard = new ClozeCard(answers.text, answers.cloze);
 
-        basic = BasicCard(answers.Question, answers.Answer);
+    //push to cloze card array. 
+    clozeCards.push(newCard);
 
-        basicCards.push(basic);
+    console.log("\nCloze card created \nComplete Question: " + answers.text
+				+ "\nCloze portion: " + answers.cloze);
 
-        console.log("\nNew card created! \nQuestion: " + answers.Question +
-            "\nAnswer: " + answers.Answer);
+    console.log(clozeCards);
+    
+    whatsNext();
 
-    })
-};
+});
+}
+
+//Prompt for next task, either review cards or add another. 
+
+var whatsNext = function() {
+    inquirer.prompt([{
+            name: 'next',
+            message: 'What would you like to do?',
+            type: 'list',
+            choices: [{
+                name: 'Build a new card'
+            }, {
+                name: 'Review cards'
+            }]
+        }])
+        .then(function(answers) {
+
+            if (answers.next === 'Build a new card') {
+                chooseCard();
+            } else if (answers.next === 'Review cards') {
+                review();
+            }
+        });
+}
+
+chooseCard();
